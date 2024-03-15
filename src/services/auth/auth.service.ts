64b789c95782629d39847109acbe2587c915c918
Fr0ntcoder <jambox228@gmail.com>
2024-03-15 16:14:23 +0800

    auth

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { axiosClassic } from '@/api/interceptors'
+
+import { TAuthForm, TAuthResponse } from './../../types/auth.types'
+import {
+	removeFromStorage,
+	saveTokenStorage,
+} from '@/services/auth/auth-token.service'
+
+export const authService = {
+	async main(type: 'login' | 'register', data: TAuthForm) {
+		const response = await axiosClassic.post<TAuthResponse>(
+			`/auth/${type}`,
+			data
+		)
+
+		if (response.data.accessToken) {
+			saveTokenStorage(response.data.accessToken)
+		}
+
+		return response
+	},
+
+	async getNewTokens() {
+		const response = await axiosClassic.post<TAuthResponse>(
+			'/auth/login/access-token'
+		)
+
+		if (response.data.accessToken) {
+			saveTokenStorage(response.data.accessToken)
+		}
+
+		return response
+	},
+	async logout() {
+		const response = await axiosClassic.post<boolean>('/auth/logout')
+
+		if (response.data) {
+			removeFromStorage()
+		}
+
+		return response
+	},
+}
